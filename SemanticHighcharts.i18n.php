51dbc0f53d46f3c57d4e8cdb09a42d691c358c8b
Kim Eik <kim@heldig.org>
2014-03-10 13:48:09 +0100

    i18n
    Change-Id: I1d73158d2e1d51a6280166fc7b98969ecf122eb6

--- SemanticHighcharts.i18n.php
+++ SemanticHighcharts.i18n.php
@@ -32,7 +32,48 @@ $messages['en'] = array(
 	'srf-hc-paramdesc-frequency-histogram-xlrotation' => 'The rotation of the x axis labels. i.e \'-90\'',
 );
 
+
 $messages['qqq'] = array(
 	'semantichighcharts-desc' => '{{desc|name=SemanticHighcharts|url=http://www.mediawiki.org/wiki/Extension:SemanticHighcharts}}',
 	'srf-hc-license-warning' => 'Warning of possible license issue.',
+	'srf-hc-format-highcharts-table-caption' => 'Caption displayed on help table',
+
+	//formats
+	'srf-hc-formatdesc-highcharts' => 'Output format parameter',
+	'srf-hc-formatdesc-hc:frequency-histogram' => 'Output format parameter',
+
+	//parameters
+	'srf-hc-paramdesc-ytitle' => 'Description of parameter ytitle',
+	'srf-hc-paramdesc-xtitle' => 'Description of parameter xtitle',
+	'srf-hc-paramdesc-title' => 'Description of parameter title',
+	'srf-hc-paramdesc-subtitle' => 'Description of parameter subtitle',
+	'srf-hc-paramdesc-minwidth' => 'Description of parameter minwidth',
+	'srf-hc-paramdesc-height' => 'Description of parameter height',
+	'srf-hc-paramdesc-frequency-histogram-bins' => 'Description of parameter bins',
+	'srf-hc-paramdesc-frequency-histogram-binrange' => 'Description of parameter binrange',
+	'srf-hc-paramdesc-frequency-histogram-xlrotation' => 'Description of parameter xlrotation',
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'semantichighcharts-desc' => 'Ermöglicht ein auf die Softwarebibliothek „Highcharts“ gestütztes Ergebnisformat',
+	'srf-hc-license-warning' => 'Dieses Ergebnisformat erfordert vor dessen Nutzung Aufmerksamkeit hinsichtlich der Lizenz. Siehe hierzu die [https://www.mediawiki.org/wiki/Extension:SemanticHighcharts#License Informationen zur Lizenz].',
+	'srf-hc-format-highcharts-table-caption' => 'Dies ist das Ergebnisformat „highcharts“. Unterhalb befindet sich eine Liste der verfügbaren Formatvarianten mitsamt deren Parametern.',
+
+	//formats
+	'srf-hc-formatdesc-highcharts' => 'Zeigt eine Liste der Formatvarianten mitsamt deren Parametern an.',
+	'srf-hc-formatdesc-hc:frequency-histogram' => 'Erzeugt die Grafik eines auf einen einzelnen Parameter gestützten Häufigkeitshistogramms.',
+
+	//parameters
+	'srf-hc-paramdesc-ytitle' => 'Der Titel der Y-Achse',
+	'srf-hc-paramdesc-xtitle' => 'Der Titel der X-Achse',
+	'srf-hc-paramdesc-title' => 'Der Titel der Grafik',
+	'srf-hc-paramdesc-subtitle' => 'Der Untertitel der Grafik',
+	'srf-hc-paramdesc-minwidth' => 'Die Mindestbreite der Grafik in Pixeln (px)',
+	'srf-hc-paramdesc-height' => 'Die Höhe der Grafik in Pixeln (px)',
+	'srf-hc-paramdesc-frequency-histogram-bins' => 'Die Anzahl der anzuzeigenden Säulen',
+	'srf-hc-paramdesc-frequency-histogram-binrange' => 'Der mit einer Säule anzuzeigende Wertebereich',
+	'srf-hc-paramdesc-frequency-histogram-xlrotation' => 'Der für die Ausrichtung der Bezeichnungen der X-Achse zu nutzende Rotationswert (bspw. „-90“)',
 );
